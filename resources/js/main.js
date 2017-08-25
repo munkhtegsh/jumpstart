86a846460c019673b791333b5c6bd46488f215e1
Munkhtegsh <munkhtegsh.m@gmail.com>
2017-08-24 19:39:15 -0700

    Navbar will disappear when user scrolls down

--- resources/js/main.js
+++ resources/js/main.js
@@ -4,7 +4,31 @@ $(document).ready(function() {
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 1000,
+  arrows: false,
+     
  });
+    
+    var userScroll = 0;
+    $(window).scroll(function() {
+    var top = $(this).scrollTop();
+    if(top - userScroll > 50){
+        var height = $('.navbar').css('height');
+        $('.navbar').animate({top:'-'+ height},150);
+        userScroll = top;
+    } else if(userScroll - top > 50) {
+        $('.navbar').animate({top: '0'}, 150);
+        userScroll = top;
+    }
+    });
+    
 });
+    
+    
 
+    
+
+
+    
+    
+    
 
